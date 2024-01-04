00c6c19baecd5f4063fc7617a6f195fdb46dd6ac
rugema3 <rugema61@gmail.com>
2024-01-04 18:00:54 +0200

    a different approach

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,15 +1,17 @@
 /**
- * Function to divide two numbers.
+ * Divides two numbers.
  *
  * @param {number} numerator - The numerator of the division.
  * @param {number} denominator - The denominator of the division.
  * @returns {number} The result of the division.
- * @throws {Error} If the denominator is equal to 0.
+ * @throws {Error} Throws an error if the denominator is equal to 0.
  */
 export default function divideFunction(numerator, denominator) {
+  // Check if the denominator is 0 to avoid division by zero
   if (denominator === 0) {
-    throw new Error('Cannot divide by 0');
+    throw Error('Cannot divide by 0');
   }
 
+  // Return the result of the division
   return numerator / denominator;
 }
