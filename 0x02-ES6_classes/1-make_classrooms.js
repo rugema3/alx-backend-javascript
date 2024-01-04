de2c1bb0d58565c83b00a887260b4d48eec0d08b
rugema3 <rugema61@gmail.com>
2024-01-04 19:37:31 +0200

    using a different approach

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,19 +1,5 @@
-// 1-make_classrooms.js
 import ClassRoom from './0-classroom';
 
-/**
- * Initialize an array of ClassRoom objects with specified sizes.
- * @returns {ClassRoom[]} - Array of ClassRoom objects.
- */
-function initializeRooms() {
-  // Create ClassRoom objects with sizes 19, 20, and 34.
-  const rooms = [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
-
-  return rooms;
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
-
-export default initializeRooms;
